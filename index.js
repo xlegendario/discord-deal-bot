48b91912073db8186d31ceef554609bbab8fcb70
xlegendario <dario.b1996@gmail.com>
2026-02-09 08:46:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildInvites
   ]
 });
 
